80fe41cf893515c5af4a4983cea69a9f0f2b94c9
Richard Wise <richard.wise@hey.com>
2022-12-26 21:41:27 -0500

    add new view modifiers

--- App/Extensions/View+Ext.swift
+++ App/Extensions/View+Ext.swift
@@ -11,12 +11,51 @@ extension View {
     func embedInNavigationStack() -> some View {
         modifier(NavigationStackEmbed())
     }
+    
+    func centerView() -> some View {
+        modifier(CenterView())
+    }
+    
+    func centerAlignText() -> some View {
+        modifier(CenterAlignText())
+    }
+    
+    func scrollsAsNeeded() -> some View {
+        modifier(ScrollAsNeeded())
+    }
 }
 
 struct NavigationStackEmbed: ViewModifier {
     func body(content: Content) -> some View {
         NavigationStack {
             content
+        }
+    }
+}
+
+struct CenterView: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .center)
+    }
+}
+
+struct CenterAlignText: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .center)
+            .multilineTextAlignment(.center)
+    }
+}
+
+struct ScrollAsNeeded: ViewModifier {
+    func body(content: Content) -> some View {
+        ViewThatFits(in: .vertical) {
+            content
+            
+            ScrollView {
+                content
+            }
         }
     }
 }
